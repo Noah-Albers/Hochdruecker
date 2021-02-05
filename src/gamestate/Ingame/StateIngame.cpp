d17f35980f7fbc045f32422a1a6d0eb27a0b0a36
Noah Albers <albers-noah@pm.me>
2021-02-05 20:04:20 +0100

    Grundlegende Spielfunktionen hinzugefügt

--- src/gamestate/Ingame/StateIngame.cpp
+++ src/gamestate/Ingame/StateIngame.cpp
@@ -1,6 +1,73 @@
 #include "StateIngame.h"
 #include "Logger.h"
+#include "Main.h"
+#include <Arduino.h>
+#include "gamestate/win/StateWin.h"
+#include "gamestate/loss/StateLoss.h"
+
+char StateIngame::getCurrentLedPin(){
+    return LED_PINS[this->winLedEnabled?this->currentStage:0];
+}
+
+void StateIngame::switchLed(){
+    // Disables the current led
+    digitalWrite(this->getCurrentLedPin(),LOW);
+
+    // Updates the led enable
+    this->winLedEnabled=!this->winLedEnabled;
+
+    // Enables the new led
+    digitalWrite(this->getCurrentLedPin(),HIGH);
+}
+
+void StateIngame::onTick(){
+    // Gets how long the timer has to wait
+    long time = (
+        (float)1/(float)(
+            diffcultyEasy?
+                this->currentStage:
+                this->winLedEnabled?this->currentStage:1
+        )
+    )*1000;
+
+    // Ensures that the time doesn't got lower than 0,2s
+    if(time<200)
+        time=200;
+
+    // Checks if the timer has already reached that limit
+    if(this->blinkTimer.hasReached(time)){
+        // Resets the timer
+        this->blinkTimer.reset();
+
+        // Switches the led
+        this->switchLed();
+    }
+}
 
 void StateIngame::onTriggerButtonPressed(){
-    logger::trace("Call from StateIngame: onSwitchButtonPressed");
+    // Disables the current led
+    digitalWrite(this->getCurrentLedPin(),LOW);
+
+    // Checks if the player has lost or won
+    if(this->currentStage == (this->winLedEnabled?AMOUNT_LEDS:1)){
+        // Checks which state should be opened next
+        if(this->winLedEnabled)
+            // Opens the win state
+            openGameState(new StateWin);
+        else
+            // Opens the loss state
+            openGameState(new StateLoss);
+        return;
+    }else
+        // In/Decreases the stage depending if the player has won
+        this->currentStage+=this->winLedEnabled?1:-1;
+
+    // Resets the timer
+    this->blinkTimer.reset();
+
+    // Resets the win led
+    this->winLedEnabled=false;
+
+    // Enables the current led
+    digitalWrite(this->getCurrentLedPin(),HIGH);
 }
--- src/gamestate/Ingame/StateIngame.h
+++ src/gamestate/Ingame/StateIngame.h
@@ -1,7 +1,24 @@
 #pragma once
 #include "../State.h"
+#include "util/Timer.h"
 
 class StateIngame : public State{
+    private:
+        // On which stage the player is on
+        int currentStage=0;
+
+        // If the correct led is blinking
+        bool winLedEnabled;
+
+        // The timer to measure the blink timing
+        Timer blinkTimer;
+
+        // Returns the GPIO-Pin of the led that is currently high
+        char getCurrentLedPin();
+        
+        // Switches the led that is currently high 
+        void switchLed();
     public:
         void onTriggerButtonPressed();
+        void onTick();
 };
