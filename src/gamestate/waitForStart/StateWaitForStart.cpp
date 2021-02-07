1f3452cdbf970a752fd6b7b602059591b82602e4
Noah Albers <albers-noah@pm.me>
2021-02-07 16:46:09 +0100

    Warteanimation für die Leds eingefügt

--- src/gamestate/waitForStart/StateWaitForStart.cpp
+++ src/gamestate/waitForStart/StateWaitForStart.cpp
@@ -1,9 +1,42 @@
 #include "StateWaitForStart.h"
 #include "Main.h"
 #include "gamestate/ingame/StateIngame.h"
+#include "CompileConfig.h"
+#include "Logger.h"
 
 StateWaitForStart::StateWaitForStart() : State("WaitForStart"){}
 
 void StateWaitForStart::onTriggerButtonPressed(){
     openGameState(new StateIngame);
+}
+
+void StateWaitForStart::onTick(){
+    // Checks if the timer is ready
+    if(!this->animationTimer.hasReached(50))
+        return;
+    
+    // Resets the timer
+    this->animationTimer.reset();
+
+    // Time it takes for the animation to complete
+    int duration = 2000; //ms
+    // The max ledc value for the leds
+    int max = 20;
+    // The min ledc value fro the leds
+    int min = 6;
+
+    // Updates every led
+    for(int i=0;i<AMOUNT_LEDS;i++){
+        // Calculates the percentage value of the current iteration.
+        float perc = (float)((millis()+(duration/AMOUNT_LEDS)*i) %(duration))/ (float)duration;
+
+        // Maps the percentage value to a scale that goes from 0 to 1 and back from 1 to 0
+        float half = (perc < .5 ? perc : (1-perc)) * 2;
+
+        // Calcualtes the actual ledc value from the max and min
+        int betwen = half*max + min;
+
+        // Updates the led
+        ledcWrite(i,betwen);
+    }
 }
--- src/gamestate/waitForStart/StateWaitForStart.h
+++ src/gamestate/waitForStart/StateWaitForStart.h
@@ -1,8 +1,13 @@
 #pragma once
 #include "../State.h"
+#include "util/Timer.h"
 
 class StateWaitForStart : public State{
+    private:
+        // Timer to update every few ms (not to frequently)
+        Timer animationTimer;
     public:
         StateWaitForStart();
         void onTriggerButtonPressed();
+        void onTick();
 };
