12c41dc3430e20263eaed39e567e310d65a52bc3
Noah Albers <albers-noah@pm.me>
2021-02-07 17:30:58 +0100

    "Fröhliches" Lichtspiel als Gewinnanimation eingefügt.

--- src/gamestate/win/StateWin.cpp
+++ src/gamestate/win/StateWin.cpp
@@ -1,11 +1,78 @@
 #include "StateWin.h"
+#include "Main.h"
+#include "Logger.h"
+#include "CompileConfig.h"
+#include "gamestate/waitForStart/StateWaitForStart.h"
 
 StateWin::StateWin() : State("Win"){}
 
-void StateWin::onTick(){
-
+void StateWin::onStateOpen(){
+    this->clearLeds();
 }
 
-void StateWin::onStateOpen(){
-    
+void StateWin::clearLeds(){
+    // Disables all leds
+    for(int i=0;i<AMOUNT_LEDS;i++)
+        ledcWrite(i,0);
+}
+
+long StateWin::getPlayTime(){
+    return this->animationTimes[this->animationStage]; //ms
+}
+
+void StateWin::onTick(){
+
+    // Checks if the timer has reached the current stages limit
+    if(this->stageTimer.hasReached(this->getPlayTime())){
+        // Resets the timer
+        this->stageTimer.reset();
+        // Starts the next stage
+        this->animationStage++;
+        // Resets the seperate animation variables
+        this->t2.reset();
+        this->i2=0;
+        // Clears all leds
+        this->clearLeds();
+
+        logger::debug("(Win) Opening next animationstate: "+String(this->animationStage));
+    }
+
+    // Checks the animation stage
+    switch(this->animationStage){
+        case 1:
+            this->animateStageOne();
+            break;
+        case 2: case 3: case 4:
+            this->animateStageTwo();
+            break;
+        case 6:
+            // Opens the waiting state again
+            openGameState(new StateWaitForStart);
+            break;
+    }
+}
+
+void StateWin::animateStageOne(){
+    // Gets a slower version of the time variable
+    long time = millis()/300;
+
+    for(int i=0;i<5;i++)
+        // Animates a blining that moves over all lamps
+        ledcWrite(i,(time+i) % 2 == 0 ? 0 : 255);
+}
+
+void StateWin::animateStageTwo(){
+    // Gets the time of one iteration per led
+    int time = (this->getPlayTime()*.8)/AMOUNT_LEDS;
+
+    // Checks if the timer has reached that limit
+    if(this->t2.hasReached(time)){
+        this->t2.reset();
+
+        // Lights up the current led
+        ledcWrite(this->i2,100); 
+
+        // Increments to the next led
+        this->i2++;
+    }
 }
--- src/gamestate/win/StateWin.h
+++ src/gamestate/win/StateWin.h
@@ -1,9 +1,34 @@
 #pragma once
 #include "../State.h"
+#include "util/Timer.h"
 
 class StateWin : public State{
+    private:
+        // Timer to help the animation
+        Timer stageTimer;
+        // The current stage of the animation
+        int animationStage=0;
+        // Holds the animation timings for all stages
+        int animationTimes[7] = {2000,5000,2000,2000,2000,4000,0};
+
+        /*
+         *   Seperat animation variables
+         */
+        
+        Timer t2;
+        int i2;
+
     public:
         StateWin();
         void onTick();
         void onStateOpen();
+
+        // Clears all leds
+        void clearLeds();
+        // Calculates the play-time of the current stage
+        long getPlayTime();
+
+        // The different animation stages
+        void animateStageOne();
+        void animateStageTwo();
 };
