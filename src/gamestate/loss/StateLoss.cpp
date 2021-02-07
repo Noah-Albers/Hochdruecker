cb1098d3b4c64dd6dfa299a34e3556c9c415b7a1
Noah Albers <albers-noah@pm.me>
2021-02-07 19:28:04 +0100

    Verlieren-Animation hinzugefügt.

--- src/gamestate/loss/StateLoss.cpp
+++ src/gamestate/loss/StateLoss.cpp
@@ -1,11 +1,38 @@
 #include "StateLoss.h"
+#include "Main.h"
+#include "CompileConfig.h"
+#include "gamestate/waitForStart/StateWaitForStart.h"
+#include "Logger.h"
 
 StateLoss::StateLoss() : State("Loss"){}
 
-void StateLoss::onTick(){
-
+void StateLoss::onStateOpen(){
+    // Enables all leds
+    for(int i=0;i<AMOUNT_LEDS; i++)
+        ledcWrite(i,100);
+    
+    logger::debug("All leds have been enabled.");
 }
 
-void StateLoss::onStateOpen(){
-    
+void StateLoss::onTick(){
+
+    // Gets the time of one iteration per led
+    int time = 3000/AMOUNT_LEDS;
+
+    // Checks if the timer has reached that limit
+    if(this->timer.hasReached(time)){
+        this->timer.reset();
+        
+        logger::debug("Triggering the disable of the next led: "+String(this->nextLed));
+
+        // Checks if the next led is outside of the scope
+        if(this->nextLed > AMOUNT_LEDS){
+            // Goes again into the wait state
+            openGameState(new StateWaitForStart);
+            return;
+        }
+
+        // Lights up the current led
+        ledcWrite(this->nextLed++,0); 
+    }
 }
--- src/gamestate/loss/StateLoss.h
+++ src/gamestate/loss/StateLoss.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "../State.h"
+#include "util/Timer.h"
 
 class StateLoss : public State{
+    private:
+        // Timer for the animation
+        Timer timer;
+        // The next led that should turn off
+        int nextLed=0;
     public:
         StateLoss();
         void onTick();
